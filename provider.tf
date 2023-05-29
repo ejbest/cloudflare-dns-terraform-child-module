361e55a41d8227963cf09c76848dba5ed8740d7c
EJ <ej.best@outlook.com>
2023-05-29 02:07:17 -0900

    first push

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">=0.13"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "3.7.0"
+    }
+  }
+}
+
+provider "cloudflare" {}
