18c39effff6d2b50060741b6167254d93de7a305
ejbest66 <126988586+ejbest66@users.noreply.github.com>
2023-08-29 08:23:56 -0400

    Update main.tf
    fixed var layout

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
-resource "cloudflare_record" "cf_record" {
-  zone_id = var.cloudflare_zone_id
+data "cloudflare_zone" "zone" {
+  name = var.cloudflare_zone_id
+}
+
+resource "cloudflare_record" "record" {
+  zone_id = data.cloudflare_zone.zone.id
   name    = var.cloudflare_name
   value   = var.cloudflare_value
   type    = var.cloudflare_type
