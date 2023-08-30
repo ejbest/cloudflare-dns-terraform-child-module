79478fb47c3102c0b392849cc148cbcdfae79804
ejbest66 <126988586+ejbest66@users.noreply.github.com>
2023-08-30 06:13:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,8 @@ data "cloudflare_zone" "zone" {
   name = var.cloudflare_zone_id
 }
 
-provider = var.provider
-
 resource "cloudflare_record" "record" {
+  provider = var.provider
   zone_id = data.cloudflare_zone.zone.id
   name    = var.cloudflare_name
   value   = var.cloudflare_value
