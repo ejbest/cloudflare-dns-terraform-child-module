f9d5bb7def5368746f3f9d508d1cf0ce01af2b5a
Erich (EJ) Best <ejbest@gmail.com>
2025-01-04 13:14:22 -0500

    adjust provider version reference

--- main.tf
+++ main.tf
@@ -11,21 +11,33 @@ data "cloudflare_zone" "zone" {
 #   proxied = var.proxied
 # }
 
+# resource "cloudflare_record" "record" {
+#   for_each = var.dns_records
+
+#   zone_id = data.cloudflare_zone.zone.id
+#   name    = each.value.name
+#   content = each.value.content
+#   type    = each.value.type
+
+#   ttl     = each.value.ttl
+#   proxied = false
+
+#   dynamic "priority" {
+#     for_each = each.value.type == "MX" && contains(keys(each.value), "priority") ? [each.value.priority] : []
+#     content {
+#       priority = priority.value
+#     }
+#   }
+# }
+
 resource "cloudflare_record" "record" {
-  for_each = var.dns_records
-
   zone_id = data.cloudflare_zone.zone.id
-  name    = each.value.name
-  content = each.value.content
-  type    = each.value.type
-
-  ttl     = each.value.ttl
+  name    = var.cloudflare_name
+  content = var.cloudflare_content
+  type    = var.cloudflare_type
+  ttl     = var.ttl
   proxied = false
 
-  dynamic "priority" {
-    for_each = each.value.type == "MX" && contains(keys(each.value), "priority") ? [each.value.priority] : []
-    content {
-      priority = priority.value
-    }
-  }
+  # Include 'priority' only if it exists in the map
+  priority = contains(keys(var), "priority") ? var.priority : null
 }
