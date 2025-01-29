21890d0fe1566cfcddf24c3f2cf2b081b6d8a43c
Erich (EJ) Best <20603184+ejbest@users.noreply.github.com>
2025-01-29 16:21:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ data "cloudflare_zone" "zone" {
 #   }
 # }
 
-resource "cloudflare_record" "record" {
+resource "cloudflare_dns_record" "record" {
   zone_id  = data.cloudflare_zone.zone.id
   name     = var.cloudflare_name
   content  = var.cloudflare_content
@@ -39,3 +39,13 @@ resource "cloudflare_record" "record" {
   proxied  = false
   priority = var.priority
 }
+
+#resource "cloudflare_record" "record" {
+#  zone_id  = data.cloudflare_zone.zone.id
+#  name     = var.cloudflare_name
+#  content  = var.cloudflare_content
+#  type     = var.cloudflare_type
+#  ttl      = var.ttl
+#  proxied  = false
+#  priority = var.priority
+#}
